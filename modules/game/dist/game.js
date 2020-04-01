cb36430af87406476c9e533610f4d3282b0f6f4c
Tobias Andersen <dappjump@gmail.com>
2020-04-01 18:34:50 +0200

    Local wallet gen

--- modules/game/dist/game.js
+++ modules/game/dist/game.js
@@ -1,6 +1,32 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Under construction'
+    message: 'Under construction',
+    state: "",
+    web3: new Web3(),
+    user: {
+    	address: ""
+    }
+  },
+  mounted: function(){
+  	console.log(wuf)
+  	let jwt = wuf.getJWT()
+  	if(jwt) {
+  		//User has login token..
+  	} else {
+  		//User does not have login token...
+  		this.onNotLoggedIn()
+  	}
+  },
+  methods: {
+  	onNotLoggedIn: function(){
+  		this.state = "initial"
+  	},
+  	createWallet: function(){
+  		this.state = "loading"
+  		this.status = "Generating account..."
+  		this.user = this.web3.eth.accounts.create();
+  		this.status = "Registering address: "+this.user.address
+  	}
   }
 })
