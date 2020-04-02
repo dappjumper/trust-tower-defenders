4fc7f5a428b81ebf34e33580a275cd4e1737c614
Tobias Andersen <dappjump@gmail.com>
2020-04-02 15:41:19 +0200

    Save encrypted wallet to localStorage, retrieve it on load

--- modules/game/dist/js/game.js
+++ modules/game/dist/js/game.js
@@ -7,7 +7,8 @@ var app = new Vue({
     web3: new Web3(),
     user: {
     	address: ""
-    }
+    },
+    error: ""
   },
   mounted: function(){
   	console.log(wuf)
@@ -18,19 +19,49 @@ var app = new Vue({
     		//User has login token..
     	} else {
     		//User does not have login token...
-    		this.onNotLoggedIn()
+    	  let memoryWallet = localStorage.getItem('ttd_enc_wallet')
+        if(!memoryWallet || memoryWallet == "null") {
+          this.state = "initial"
+        } else {
+          this.user = JSON.parse(memoryWallet)
+          this.state = "decryptWallet"
+        }
     	}
     }.bind(this),1)
   },
   methods: {
-  	onNotLoggedIn: function(){
-  		this.state = "initial"
-  	},
+    setError: function(string){
+      this.error = string
+    },
+    deleteWallet: function(){
+      localStorage.setItem('ttd_enc_wallet', null)
+      this.state = "initial"
+      this.error = ""
+      this.status = ""
+    },
   	createWallet: function(){
   		this.state = "loading"
   		this.status = "Generating account..."
   		this.user = this.web3.eth.accounts.create();
   		this.state = "encryptNewWallet"
-  	}
+  	},
+    decryptWallet: function(){
+      let pass = document.querySelector('#decryptPassword').value
+      if(!pass) return this.setError('Please enter password')
+      this.state = "loading"
+      this.status = "Decoding wallet..."
+    },
+    encryptWallet: function(){
+      let pass = document.querySelector('#encryptPassword').value
+      let pass_v = document.querySelector('#encryptPasswordVerify').value
+      if(!pass) return this.setError('Please enter password')
+      if(!pass_v) return this.setError('Please re-type password')
+      if(pass !== pass_v) return this.setError('passwords do not match')
+      this.state = "loading"
+      this.status = "Encrypting your password"
+      setTimeout(function(){
+        localStorage.setItem('ttd_enc_wallet',JSON.stringify(this.user.encrypt(pass)));
+      }.bind(this),1)
+    }
   }
 })
