b2adffa30c119a51d6ea87dd7082ca494f7aa921
Tobias Andersen <dappjump@gmail.com>
2020-04-03 12:09:31 +0200

    Handle encrypt/decrypt less wonky

--- modules/game/dist/js/game.js
+++ modules/game/dist/js/game.js
@@ -2,6 +2,7 @@ var app = new Vue({
   el: '#app',
   data: {
     message: 'Under construction',
+    nonce: 0,
     state: "",
     display: "",
     web3: new Web3(),
@@ -33,6 +34,11 @@ var app = new Vue({
     setError: function(string){
       this.error = string
     },
+    getChallenge: function(user){
+      this.state = ""
+      this.state = "loading"
+      this.status = "Contacting server..."
+    },
     beginLogin: function(strategy) {
       switch(strategy) {
         case 'local':
@@ -52,24 +58,39 @@ var app = new Vue({
   		this.user = this.web3.eth.accounts.create();
   		this.state = "encryptNewWallet"
   	},
+    decryptV3: function(user, password){
+      this.state = "loading"
+      this.status = "Decrypting..."
+      return new Promise(function(resolve, reject){
+        setTimeout(function(){
+          let res = this.web3.eth.accounts.decrypt(user, password)
+          resolve(res)
+        }.bind(this),1)
+      }.bind(this))
+    },
+    encryptV3: function(user, password){
+      this.state = "loading"
+      this.status = "Encrypting..."
+      return new Promise(function(resolve, reject){
+        setTimeout(function(){
+          let res = this.web3.eth.accounts.encrypt(user.privateKey, password)
+          resolve(res)
+        }.bind(this),1)
+      }.bind(this))
+    },
     decryptWallet: function(){
       let pass = document.querySelector('#decryptPassword').value
       if(!pass) return this.setError('Please enter password')
       this.state = "loading"
       this.status = "Decoding wallet..."
-      setTimeout(function(){
-        try {
-          this.user = this.web3.eth.accounts.decrypt(this.user, pass);
-          this.state = ""
-          this.status = "Contacting server..."
-          setTimeout(function(){
-            this.state = "loading"
-          }.bind(this),1)
-        } catch(e){
+      this.decryptV3(this.user, pass)
+        .then((decrypted)=>{
+           this.getChallenge(decrypted)
+        })
+        .catch(()=>{
           this.state = "decryptWallet"
-          return this.error = "Incorrect password, please try again."
-        }
-      }.bind(this),1)
+          this.error = "Incorrect password, please try again."
+        })
     },
     encryptWallet: function(){
       let pass = document.querySelector('#encryptPassword').value
@@ -78,11 +99,15 @@ var app = new Vue({
       if(!pass_v) return this.setError('Please re-type password')
       if(pass !== pass_v) return this.setError('passwords do not match')
       this.state = "loading"
-      this.status = "Encrypting your password"
-      setTimeout(function(){
-        localStorage.setItem('ttd_enc_wallet', JSON.stringify(this.web3.eth.accounts.encrypt(this.user.privateKey, pass)))
-        this.beginLogin('local')
-      }.bind(this),1)
+      this.status = "Encrypting your password..."
+      this.encryptV3(this.user, pass)
+        .then(function(user){
+          localStorage.setItem('ttd_enc_wallet', JSON.stringify(user))
+          this.getChallenge(this.user)
+        }.bind(this))
+        .catch(function(e){
+          this.state = "initial"
+        }.bind(this))
     }
   }
 })
